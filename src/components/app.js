149211c177aa2981499bb02938c9fd5f872dba3d
yoyofrog <tomfeng100@gmail.com>
2020-10-06 15:57:31 -0700

    add init files

---
+++ src/components/app.js
@@ -0,0 +1,10 @@
+import React, { Component} from 'react'
+import { Button } from 'antd-mobile'
+
+export default class App extends Component {
+    render() {
+        return(
+            <div><Button type='primary'>asdf</Button></div>
+        )
+    }
+}
