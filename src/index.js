d6b38c6db0217ba600d76d11ae16d878543ddc0e
yoyofrog <tomfeng100@gmail.com>
2020-10-06 17:04:52 -0700

    add init redux files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HashRouter, Route, Switch} from 'react-router-dom'
+import {Provider} from 'react-redux'
 
-import App from './components/app';
+import store from './redux/store'
+import Login from './container/login/login'
+import Register from "./container/register/register";
+import Main from "./container/main/main";
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <Provider store={store}>
+        <HashRouter>
+            <Switch>
+                <Route path='/login' component={Login}></Route>
+                <Route path='/register' component={Register}></Route>
+                <Route component={Main}></Route>
+            </Switch>
+        </HashRouter>
+    </Provider>
+    ), document.getElementById('root'));
 
